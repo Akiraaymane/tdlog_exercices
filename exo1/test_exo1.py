10aa38e1a406947550c426f4e104b8c70b4cb691
Akiraaymane <aymanedh2211@gmail.com>
2023-10-17 22:10:10 +0100

    Bien reçu Adam

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -8,4 +8,4 @@ class Exo1Test(unittest.TestCase):
     def test_item_construction(self):
         item = Item(10, 20)
 
-        self.assertEqual(20, item.weight)
+        self.assertEqual(30, item.weight)
